c324e674bcbe7034fe369cdc9417219d14042b11
WIN-U0AGMKOLSIH\user <awagadrog@gmail.com>
2020-12-08 14:16:24 +0300

    hibernate with xml

---
+++ src/main/java/awagadro/studentApp/entity/Subject.java
@@ -0,0 +1,33 @@
+package awagadro.studentApp.entity;
+
+import java.util.Set;
+
+public class Subject {
+	private Long subjectId;
+    private String subjectName;
+    private Set<Profession> professionList;
+ 
+    public Set<Profession> getProfessionList() {
+        return professionList;
+    }
+ 
+    public void setProfessionList(Set<Profession> professionList) {
+        this.professionList = professionList;
+    }
+ 
+    public Long getSubjectId() {
+        return subjectId;
+    }
+ 
+    public void setSubjectId(Long subjectId) {
+        this.subjectId = subjectId;
+    }
+ 
+    public String getSubjectName() {
+        return subjectName;
+    }
+ 
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+}
